f0e7a59316b96956cb35d3362bac8fe0fe9edb1b
clement.ab62@gmail.com <clement.ab62@gmail.com>
2023-04-26 10:30:23 +0200

    ok

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
- const lorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ultricies mi facilisis libero sollicitudin vulputate. Mauris facilisis vestibulum nisi, at viverra aridcu interdum .' 
+ 
+ 
+const lorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ultricies mi facilisis libero sollicitudin vulputate. Mauris facilisis vestibulum nisi, at viverra aridcu interdum .' 
+const titre1 = '25 avril 2023'
+const titre2 = 'lorem ipsum dolor sit amret'
+
+
  var articletext='';
 
 function ajouttxt(text,images,titre1,titre2){
@@ -15,12 +21,12 @@ return `
 </div>` 
 }
 
-articletext = articletext + ajouttxt(lorem,'img/image_001.jpg',"25 avril 2023","lorem ipsum dolor sit amret");
-articletext = articletext + ajouttxt(lorem,'img/image_002.jpg',"25 avril 2023","lorem ipsum dolor sit amret");
-articletext = articletext + ajouttxt(lorem,'img/image_003.jpg',"25 avril 2023","lorem ipsum dolor sit amret");
-articletext = articletext + ajouttxt(lorem,'img/image_001.jpg',"25 avril 2023","lorem ipsum dolor sit amret");
-articletext = articletext + ajouttxt(lorem,'img/image_002.jpg',"25 avril 2023","lorem ipsum dolor sit amret");
-articletext = articletext + ajouttxt(lorem,'img/image_003.jpg',"25 avril 2023","lorem ipsum dolor sit amret");
+articletext = articletext + ajouttxt(lorem,'img/image_001.jpg',titre1,titre2);
+articletext = articletext + ajouttxt(lorem,'img/image_002.jpg',titre1,titre2);
+articletext = articletext + ajouttxt(lorem,'img/image_003.jpg',titre1,titre2);
+articletext = articletext + ajouttxt(lorem,'img/image_001.jpg',titre1,titre2);
+articletext = articletext + ajouttxt(lorem,'img/image_002.jpg',titre1,titre2);
+articletext = articletext + ajouttxt(lorem,'img/image_003.jpg',titre1,titre2);
 
 
 document.getElementById("article").innerHTML=articletext; 
